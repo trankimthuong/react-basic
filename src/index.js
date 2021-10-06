13873b76f61388e2da76b4181b9de4fa93c7b91f
Tran Kim Thuong <44781952+trankimthuong@users.noreply.github.com>
2021-10-06 21:39:23 +0700

    add bootstrap, jquery

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import DemoComponent from './DemoComponent'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <DemoComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );

0ab3259a01f9e235145d15d9d72b558560802a0b
Tran Kim Thuong <44781952+trankimthuong@users.noreply.github.com>
2021-10-06 20:44:25 +0700

    demo class component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DemoComponent from './DemoComponent'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <DemoComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
